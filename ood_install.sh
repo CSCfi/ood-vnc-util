c607e8fe34ce0df99896886972c8ad2994471376
Robin Karlsson <robin.karlsson@csc.fi>
2021-09-22 15:27:17 +0300

    Hide app selection when no app is selected

--- ood_install.sh
+++ ood_install.sh
@@ -1,1 +1,3 @@
-ln -fns ../../../deps/util/forms/form_validated.js form.js
+# TODO: Make ood-util update form.js when form_validated.js changes
+cp script.js form.js
+cat ../../../../../deps/util/forms/form_validated.js >> form.js
