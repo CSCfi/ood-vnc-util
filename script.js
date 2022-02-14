3271df2ef72cb22b05b85352983a99ee52ed98a9
Robin Karlsson <robin.karlsson@csc.fi>
2022-02-14 12:07:04 +0200

    Fix flashing page elements

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
+(function () {
   const desktop_environment_input = $("#batch_connect_session_context_desktop");
   desktop_environment_input.change(update_app_input);
   update_app_input();
-});
+})();
 
 function update_app_input() {
   const desktop_environment_input = $("#batch_connect_session_context_desktop");
   const app_input = $("#batch_connect_session_context_app");
-  
+
   if (desktop_environment_input.val() === "") {
     app_input.parent().show();
   } else {
