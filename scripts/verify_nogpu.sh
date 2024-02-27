ed5f6ac9a7e06f5608a3058be91344de45101f17
Robin Karlsson <robin.karlsson@csc.fi>
2024-02-27 15:12:17 +0200

    Add dialog title

--- scripts/verify_nogpu.sh
+++ scripts/verify_nogpu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DIALOG_TEXT="The application you are trying to launch does not have GPU accelerated, but you are on a GPU partition. Are you sure you want to continue?"
+DIALOG_TITLE="Are you sure?"
 
 {
   [[ "$SLURM_JOB_PARTITION" != *gpu* ]] || \
-  singularity exec /appl/opt/ood/$SLURM_OOD_ENV/vnc_images/xfce.sif zenity --question --text="$DIALOG_TEXT";
+  singularity exec /appl/opt/ood/$SLURM_OOD_ENV/vnc_images/xfce.sif zenity --question --title="$DIALOG_TITLE" --text="$DIALOG_TEXT";
 } || exit 1
